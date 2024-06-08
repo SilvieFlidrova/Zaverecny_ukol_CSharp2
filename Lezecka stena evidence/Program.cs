edd4a17e23d0409e38166f25b14576f121e685af
SilvieFlidrova <flidrova@spsostrov.cz>
2024-06-08 14:40:17 +0200

    vytvoreni trid lezcu, cest a pokusu o zdolani

---
+++ Lezecka stena evidence/Program.cs
@@ -0,0 +1,116 @@
+﻿namespace Lezecka_stena_evidence
+{
+    // evidence lezců, cest a pokusů o zdolání
+
+    // Výčtový typ pro obtížnost lezeckých cest
+    public enum Obtiznost
+    { B4a, B4b, B4c, B5a, B5b, B5c, B6a, B6b, B6c, B7a, B7b };
+
+    // Třída pro lezeckou trasu
+    public class LezeckaTrasa
+    {
+        public string Nazev { get; set; }
+        public string Autor { get; set; }
+        public Obtiznost Obtiznost { get; set; }
+        public double Delka { get; set; }
+
+        public LezeckaTrasa(string nazev, string autor, Obtiznost obtiznost, double delka)
+        {
+            Nazev = nazev;
+            Autor = autor;
+            Obtiznost = obtiznost;
+            Delka = delka;
+        }
+    }
+
+    // Třída pro lezce
+    public class Lezec
+    {
+        public string Jmeno { get; set; }
+        public int Vek { get; set; }
+        public double Vyska { get; set; }
+
+        public Lezec(string jmeno, int vek, double vyska)
+        {
+            Jmeno = jmeno;
+            Vek = vek;
+            Vyska = vyska;
+        }
+    }
+
+
+    // Potomek třída pro nezlelezce             podminka pro zapsani pokusu vysloveny souhlas ZZ?
+    public class Dite : Lezec
+    {
+        public string JmenoZakonnehoZastupce { get; set; }
+        public bool Souhlas { get; set; }
+
+        public Dite(string jmeno, int vek, double vyska, string jmenoZZ, bool souhlas)
+         : base(jmeno, vek, vyska)
+        {
+            JmenoZakonnehoZastupce = jmenoZZ;
+            Souhlas = souhlas;
+        }
+    }
+
+
+    // Třída pro evidenci lezeckých tras a lezců
+    public class EvidencniZaznam
+    {
+        private Dictionary<LezeckaTrasa, List<(Lezec, DateTime, bool)>> evidencniZaznamy;
+
+        public EvidencniZaznam()
+        {
+            evidencniZaznamy = new Dictionary<LezeckaTrasa, List<(Lezec, DateTime, bool)>>();
+        }
+
+        public void PridejEvidencniZaznam(Lezec lezec, LezeckaTrasa trasa, DateTime datum, bool uspech)
+        {
+            if (!evidencniZaznamy.ContainsKey(trasa))
+            {
+                evidencniZaznamy[trasa] = new List<(Lezec, DateTime, bool)>();
+            }
+            evidencniZaznamy[trasa].Add((lezec, datum, uspech));
+        }
+
+        public void ZobrazEvidencniZaznamy()
+        {
+            foreach (var trasa in evidencniZaznamy)
+            {
+                Console.WriteLine($"Lezecka Trasa: {trasa.Key.Nazev}, Autor: {trasa.Key.Autor}, Obtiznost: {trasa.Key.Obtiznost}, Delka: {trasa.Key.Delka}");
+                Console.WriteLine("Historie Lezeni:");
+                foreach (var zaznam in trasa.Value)
+                {
+                    Console.WriteLine($" - Lezec: {zaznam.Item1.Jmeno}, Datum: {zaznam.Item2}, Uspech: {zaznam.Item3}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Vytvoření lezců
+            Lezec lezec1 = new Lezec("Jan", 25, 175);
+            Lezec lezec2 = new Lezec("Alice", 30, 160);
+            Lezec lezec3 = new Dite("Peta", 13, 152, "Petr Novák", true);
+
+            // Vytvoření lezeckých tras
+            LezeckaTrasa trasa1 = new LezeckaTrasa("Trasa A", "Autor 1", Obtiznost.B4b, 15);
+            LezeckaTrasa trasa2 = new LezeckaTrasa("Trasa B", "Autor 2", Obtiznost.B5b, 14.5);
+
+            // Vytvoření evidence
+            EvidencniZaznam evidencniZaznam = new EvidencniZaznam();
+
+            // Přidání záznamů do evidence
+            evidencniZaznam.PridejEvidencniZaznam(lezec1, trasa1, DateTime.Now, true);
+            evidencniZaznam.PridejEvidencniZaznam(lezec2, trasa1, DateTime.Now, false);
+            evidencniZaznam.PridejEvidencniZaznam(lezec1, trasa2, DateTime.Now, true);
+
+            // Zobrazení evidence
+            evidencniZaznam.ZobrazEvidencniZaznamy();
+        }
+    }
+}
